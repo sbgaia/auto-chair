afd1afb555d37f213636c028eaee7fa8ea64fd50
Sebastiano <gaia.franseba@gmail.com>
2026-03-02 20:53:55 +0100

    feat(api): Use environment variable for OpenAlex API key

--- auto-chair-openalex/src/main.rs
+++ auto-chair-openalex/src/main.rs
@@ -1,12 +1,13 @@
-use std::collections::HashMap;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {    
+    let openalex_api_key = env::var("OPENALEX_API_KEY")?;
     let client_builder = reqwest::ClientBuilder::new();
     let client = client_builder.build()?;
 
     let mut url = reqwest::Url::parse("https://api.openalex.org/authors/https://orcid.org/0000-0002-9451-1957")?;
-    url.set_query(Some("api_key=odrzvqHhJTTgzgrPRLrnZj"));
+    url.set_query(Some(&format!("api_key={}", openalex_api_key)));
 
     let resp = client.get(url).send().await?;
 
